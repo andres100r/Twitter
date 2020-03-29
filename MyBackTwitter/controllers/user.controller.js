ba2e4b581930b3009139fe30b776e5b62577af09
andres felipe <andres100r@hotmail.com>
2020-03-29 12:39:42 -0500

    folder database

--- MyBackTwitter/controllers/user.controller.js
+++ MyBackTwitter/controllers/user.controller.js
@@ -1,28 +1,44 @@
-const dbManager = require ('../database.config/db.manager');
+
+
+const dbManager = require ('../database/db.manager');
 
 /**
  * Creation of an user
  * @param {*} userObject JSON Object with User information
  */
-async function createUser (req, res) {    
-    console.log(req.body);
-    if (!req.body) {// valida que el cuerpo no este vacio
-        res.status(400).send({ message: "Request body is empty!!!!" });
+async function createUser (req, res) {
+    
+    // CHECK IF THE REQUEST BODY IS EMPTY
+    if (!req.body) {
+        res.status(400).send({
+          message: "Request body is empty!!!!"
+        });
         return;
     }
-    const newUserObject = {// creacion del objeto
+    
+    // CREATING THE OBJECT TO PERSIST
+    const newUserObject = {
         username: req.body.username,
         creation_date: req.body.creation_date
     }
-    dbManager.User.create(newUserObject).then (// EXECUTING THE CREATE QUERY - INSERT THE OBJECT INTO DATABASE 
-        data => { res.send (data); }
+    
+    // EXECUTING THE CREATE QUERY - INSERT THE OBJECT INTO DATABASE 
+    dbManager.User.create(newUserObject).then (
+        data => {
+            res.send (data);
+        }
     ).catch (
-        error => {
-            console.log(error);// imprime el error en la consola
-            res.status(500).send({ message: "Some error occurred" });// muestra el mensaje de error
+        e => {
+            // Print error on console
+            console.log(e);
+            // Send error message as a response 
+            res.status(500).send({
+                message: "Some error occurred"
+            });
         }
     );
-}
+    }
 
+    
 
-exports.createUser=createUser;
+    exports.createUser = createUser
--- MyBackTwitter/routes/users.route.js
+++ MyBackTwitter/routes/users.route.js
@@ -1,9 +1,15 @@
+
 var express = require('express');
 var router = express.Router();
+//const userController = require ('../controllers/user.controller'); 
+const userController = require ('../controllers/user.controller'); 
 
-/* GET users listing. */
+/* GET users listing. 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+*/
 
+/* GET users listing. */
+//router.get('/', userController.createUser);
 module.exports = router;
