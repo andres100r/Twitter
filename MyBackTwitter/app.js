31fd3465f267389ba5a8b963aa9e067e1d0a6c1b
andres felipe <andres100r@hotmail.com>
2020-03-29 12:19:53 -0500

    use.route

--- MyBackTwitter/app.js
+++ MyBackTwitter/app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users.controller');
+var usersRouter = require('./routes/users.route');
 
 
 //import DBmanager
